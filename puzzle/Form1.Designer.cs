0cc456abdb0be868f20c2d424102ff9c5843b18d
Jyun-Yi Huang <JYUNYI_HUANG@UBIQCONN.COM>
2019-12-03 18:06:07 +0800

    first commit

---
+++ puzzle/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(534, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(254, 388);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(237, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(342, 283);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ puzzle/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int itemX, itemY;
+        private int mousX, mousY;
+        private bool isMoving = false;
+        private int boundX, boundY;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            boundX = this.Width;
+            boundY = this.Height;
+            pictureBox1.MouseDown += PictureBox1_MouseDown;
+            pictureBox1.MouseMove += PictureBox1_MouseMove;
+            pictureBox1.MouseUp += PictureBox1_MouseUp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("pressed", "title");
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            isMoving = false;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            if(isMoving)
+            {
+                int tX = Cursor.Position.X;
+                int tY = Cursor.Position.Y;
+                ((PictureBox)sender).Left = itemX + (tX - mousX);
+                ((PictureBox)sender).Top  = itemY + (tY - mousY);
+
+                if ((((PictureBox) sender).Left + ((PictureBox)sender).Width ) > (boundX-35) ) // right limit
+                {
+                    isMoving = false;
+                    ((PictureBox)sender).Left = boundX - 35 - ((PictureBox)sender).Width;
+                }
+                if(((PictureBox)sender).Left < 0 + 20) // left limit
+                {
+                    isMoving = false;
+                    ((PictureBox)sender).Left = (0 + 20);
+                }
+
+                if(((PictureBox)sender).Top < (0 + 30)) // up limit
+                {
+                    isMoving = false;
+                    ((PictureBox)sender).Top = (0 + 30);
+
+                }
+                if ( (((PictureBox)sender).Top + ((PictureBox)sender).Height) > (boundY - 60)) // down limit
+                {
+                    isMoving = false;
+                    ((PictureBox)sender).Top = (boundY - 60 - ((PictureBox)sender).Height);
+
+                }
+
+            }
+        }
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //throw new NotImplementedException();
+            itemX = ((PictureBox)sender).Left;
+            itemY = ((PictureBox)sender).Top;
+            mousX = Cursor.Position.X;
+            mousY = Cursor.Position.Y;
+            isMoving = true;
+        }
+    }
+}
